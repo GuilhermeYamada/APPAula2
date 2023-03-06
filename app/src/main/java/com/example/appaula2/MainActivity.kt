980c2b4c95ac721c47afc74d40b05346fbd1d69b
GuilhermeYamada <guilherme.barbaresco@etec.sp.gov.br>
2023-03-06 10:59:47 -0300

    APPAula2

---
+++ app/src/main/java/com/example/appaula2/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.appaula2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //Variaveis de inicialização
+        val edtNumeroUm:EditText = findViewById(R.id.edtNumeroUm)
+        val edtNumeroDois:EditText = findViewById(R.id.edtNumeroDois)
+        val btnCalcular:Button = findViewById(R.id.btnCalcular)
+
+        /* Calculo a ser realizado */
+        btnCalcular.setOnClickListener {
+            //val Toast = Toast.makeText(this, "O botão foi clicado", Toast.LENGTH_SHORT)
+            //Toast.show()
+            calcular()
+        }
+    }
+    private fun calcular(){
+        val total: Int
+        total = 10 + 6
+        val Toast = Toast.makeText(this, total.toString(), Toast.LENGTH_SHORT)
+        Toast.show()
+    }
+}
